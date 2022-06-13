c3a89aa650c498332e4ffa1e890d5d904701bb7f
TaiZDTaiwan <raphael.huangdubois@gmail.com>
2022-06-13 17:18:10 +0200

    Initial Commit

---
+++ GoVelib/GoVelibApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoVelibApp.swift
+//  GoVelib
+//
+//  Created by Raphaël Huang-Dubois on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct GoVelibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
